1f6f24addad064e913e2d1f6e02741f28a0ff641
rlovelessiii <richardlovelessiii@gmail.com>
2021-01-30 18:24:41 -0500

    Forgot SideNav config file

---
+++ src/config/SideNav/nav-menu.js
@@ -0,0 +1,22 @@
+export const NavMenu = [
+  {
+    name: 'Projects',
+    slug: '#',
+  },
+  {
+    name: 'Blog',
+    slug: '/blog/',
+  },
+  {
+    name: 'Contact',
+    slug: '#',
+  },
+  {
+    name: 'About',
+    slug: '#',
+  },
+  {
+    name: 'Settings',
+    slug: '#'
+  }
+]
