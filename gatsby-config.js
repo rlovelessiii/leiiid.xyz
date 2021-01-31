d1b04e0ab18763349bdcbb40f89efa9ce248b091
rlovelessiii <richardlovelessiii@gmail.com>
2021-01-31 15:04:29 -0500

    Updated config for remark plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,84 @@ module.exports = {
     }
   },
   plugins: [
-    'gatsby-plugin-emotion'
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-emotion',
+      options: {
+        sourceMap: true,
+        autoLabel: 'dev-only',
+        labelFormat: '[local]',
+        cssPropOptimization: true
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          'gatsby-remark-autolink-headers',
+          {
+            resolve: `gatsby-remark-table-of-contents`,
+            options: {
+              exclude: "Table of Contents",
+              tight: true,
+              ordered: false,
+              fromHeading: 1,
+              toHeading: 6,
+              className: "table-of-contents"
+            },
+          },
+          {
+            resolve: 'gatsby-remark-mermaid',
+            options: {
+              language: 'mermaid',
+              theme: 'neutral',
+              viewport: {
+                width: 700,
+                height: 200
+              },
+              mermaidOptions: {
+                //themeCSS: ".node rect { fill: cornflowerblue; }"
+              }
+            }
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: " -> ",
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+              languageExtensions: [
+                {
+                  language: "superscript",
+                  extend: "javascript",
+                  definition: {
+                    superscript_types: /(SuperType)/,
+                  },
+                  insertBefore: {
+                    function: {
+                      superscript_keywords: /(superif|superelse)/,
+                    },
+                  },
+                },
+              ],
+              prompt: {
+                user: "user",
+                host: "localhost",
+                global: true,
+              },
+              escapeEntities: {}
+            }
+          }
+        ]
+      }
+    }
   ]
 }
