76a8838733bfe8c030bccb48689f41da9529e528
rlovelessiii <richardlovelessiii@gmail.com>
2021-01-30 22:16:32 -0500

    Added siteMetadata and emotion plugin

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Leiiid.xyz',
+    description: 'Official Website for Loveless Engineering | Design | Development | Deployment',
+    siteUrl: 'https://leiiid.xyz',
+    copyright: {
+      author: 'Richard C. Loveless III',
+      year: '2021',
+    }
+  },
+  plugins: [
+    'gatsby-plugin-emotion'
+  ]
+}
