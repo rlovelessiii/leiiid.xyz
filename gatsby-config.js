358cf694fa42b5d9a2dc7f925f7fd78a1056bbe6
rlovelessiii <richardlovelessiii@gmail.com>
2021-02-15 17:02:56 -0500

    add new dependencies and updated gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: 'Leiiid.xyz',
@@ -17,6 +21,18 @@ module.exports = {
       }
     },
     {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'GitHub',
+        fieldName: 'github',
+        url: 'https://api.github.com/graphql',
+        headers: {
+          Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
+        },
+        fetchOptions: {},
+      },
+    },
+    {
       resolve: 'gatsby-plugin-emotion',
       options: {
         sourceMap: true,
@@ -26,21 +42,28 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           'gatsby-remark-autolink-headers',
           'gatsby-remark-responsive-iframe',
           {
-            resolve: `gatsby-remark-table-of-contents`,
+            resolve: 'gatsby-remark-table-of-contents',
             options: {
-              exclude: "Table of Contents",
+              exclude: 'Table of Contents',
               tight: true,
               ordered: false,
               fromHeading: 1,
-              toHeading: 6,
-              className: "table-of-contents"
+              toHeading: 4,
+              className: 'table-of-contents'
             },
+          },
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'noreferrer noopener'
+            }
           },
           {
             resolve: 'gatsby-remark-mermaid',
@@ -57,17 +80,17 @@ module.exports = {
             }
           },
           {
-            resolve: `gatsby-remark-prismjs`,
+            resolve: 'gatsby-remark-prismjs',
             options: {
-              classPrefix: "language-",
-              inlineCodeMarker: " -> ",
+              classPrefix: 'language-',
+              inlineCodeMarker: ' -> ',
               aliases: {},
               showLineNumbers: false,
               noInlineHighlight: false,
               languageExtensions: [
                 {
-                  language: "superscript",
-                  extend: "javascript",
+                  language: 'superscript',
+                  extend: 'javascript',
                   definition: {
                     superscript_types: /(SuperType)/,
                   },
@@ -79,8 +102,8 @@ module.exports = {
                 },
               ],
               prompt: {
-                user: "user",
-                host: "localhost",
+                user: 'user',
+                host: 'localhost',
                 global: true,
               },
               escapeEntities: {}
